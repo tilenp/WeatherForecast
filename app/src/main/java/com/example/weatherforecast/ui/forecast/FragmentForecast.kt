317f2c989aa3ba0834a9b53fe3f7653e4033286e
Tilen Prepeluh <tpr@shpock.com>
2020-12-13 09:54:44 +0100

    FragmentForecast UI

--- app/src/main/java/com/example/weatherforecast/ui/forecast/FragmentForecast.kt
+++ app/src/main/java/com/example/weatherforecast/ui/forecast/FragmentForecast.kt
@@ -1,10 +1,11 @@
-package com.example.weatherforecast.ui
+package com.example.weatherforecast.ui.forecast
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.FragmentForecastBinding
 
 class FragmentForecast : Fragment() {
@@ -19,7 +20,18 @@ class FragmentForecast : Fragment() {
     ): View? {
         _binding = FragmentForecastBinding.inflate(inflater, container, false)
         val view = binding.root
+        setUpUI()
         return view
+    }
+
+    private fun setUpUI() {
+        with(binding) {
+            weatherStateTextView.text =
+                String.format(getString(R.string.weather_state_format), "Sunny")
+            minTempTextView.text = String.format(getString(R.string.min_temp_format), 26)
+            maxTempTextView.text = String.format(getString(R.string.max_temp_format), 30)
+            windSpeedTextView.text = String.format(getString(R.string.wind_speed_format), 5)
+        }
     }
 
     override fun onDestroyView() {
