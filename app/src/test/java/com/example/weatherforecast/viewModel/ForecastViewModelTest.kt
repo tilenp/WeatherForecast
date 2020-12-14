a164b95f6775c30d071d1eff29b7697f8eab9cb9
Tilen Prepeluh <tpr@shpock.com>
2020-12-15 00:57:47 +0100

    fix tests

--- app/src/test/java/com/example/weatherforecast/viewModel/ForecastViewModelTest.kt
+++ app/src/test/java/com/example/weatherforecast/viewModel/ForecastViewModelTest.kt
@@ -5,7 +5,9 @@ import com.example.weatherforecast.repository.Forecast
 import com.example.weatherforecast.repository.ForecastRepository
 import com.example.weatherforecast.repository.Location
 import com.example.weatherforecast.utils.TestSchedulerProvider
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Completable
 import io.reactivex.Observable
 import org.junit.Test
@@ -27,86 +29,65 @@ class ForecastViewModelTest {
         viewModel = ForecastViewModel(forecastRepository, eventAggregator, TestSchedulerProvider())
     }
 
-//    @Test
-//    fun when_location_is_selected_loading_state_is_set() {
-//        // arrange
-//        setUp(
-//            selectedLocationObservable = Observable.just(Location()),
-//            updateForecastCompletable = Completable.never()
-//        )
-//
-//        // act
-//        viewModel.start()
-//
-//        // assert
-//        viewModel.getUIState()
-//            .test()
-//            .assertValue(UIState.Loading)
-//            .assertNoErrors()
-//            .dispose()
-//
-//        viewModel.stop()
-//    }
-//
-//    @Test
-//    fun when_forecast_update_fails_error_state_is_set() {
-//        // arrange
-//        setUp(
-//            selectedLocationObservable = Observable.just(Location()),
-//            updateForecastCompletable = Completable.error(Throwable())
-//        )
-//
-//        // act
-//        viewModel.start()
-//
-//        // assert
-//        viewModel.getUIState()
-//            .test()
-//            .assertValue(UIState.Error(R.string.update_forecast_error))
-//            .dispose()
-//
-//        viewModel.stop()
-//    }
-//
-//    @Test
-//    fun when_forecast_is_emitted_success_state_is_set() {
-//        // arrange
-//        setUp(
-//            selectedLocationObservable = Observable.just(Location()),
-//            forecastObservable = Observable.just(Forecast())
-//        )
-//
-//        // act
-//        viewModel.getTomorrowForecast()
-//            .test()
-//            .dispose()
-//
-//        // assert
-//        viewModel.getUIState()
-//            .test()
-//            .assertValue(UIState.Success)
-//            .assertNoErrors()
-//            .dispose()
-//    }
-//
-//    @Test
-//    fun when_database_query_fails_error_state_is_set() {
-//        // arrange
-//        setUp(
-//            selectedLocationObservable = Observable.just(Location()),
-//            forecastObservable = Observable.error(Throwable())
-//        )
-//
-//        // act
-//        viewModel.getTomorrowForecast()
-//            .test()
-//            .dispose()
-//
-//        // assert
-//        viewModel.getUIState()
-//            .test()
-//            .assertValue(UIState.Error(R.string.database_error))
-//            .assertNoErrors()
-//            .dispose()
-//    }
+    @Test
+    fun when_forecast_update_fails_error_state_is_set() {
+        // arrange
+        setUp(
+            selectedLocationObservable = Observable.just(Location()),
+            updateForecastCompletable = Completable.error(Throwable())
+        )
+
+        // act
+        viewModel.start()
+
+        // assert
+        viewModel.getUIState()
+            .test()
+            .assertValue(UIState.Error(R.string.update_forecast_error))
+            .dispose()
+
+        viewModel.stop()
+    }
+
+    @Test
+    fun when_forecast_is_emitted_success_state_is_set() {
+        // arrange
+        setUp(
+            selectedLocationObservable = Observable.just(Location()),
+            forecastObservable = Observable.just(Forecast())
+        )
+
+        // act
+        viewModel.getTomorrowForecast()
+            .test()
+            .dispose()
+
+        // assert
+        viewModel.getUIState()
+            .test()
+            .assertValue(UIState.QuerySuccess)
+            .assertNoErrors()
+            .dispose()
+    }
+
+    @Test
+    fun when_database_query_fails_error_state_is_set() {
+        // arrange
+        setUp(
+            selectedLocationObservable = Observable.just(Location()),
+            forecastObservable = Observable.error(Throwable())
+        )
+
+        // act
+        viewModel.getTomorrowForecast()
+            .test()
+            .dispose()
+
+        // assert
+        viewModel.getUIState()
+            .test()
+            .assertValue(UIState.Error(R.string.database_error))
+            .assertNoErrors()
+            .dispose()
+    }
 }
